34014cbedb2b5183dfbdfb2790f60d388b61406e
brendap21 <bpna16100224@gmail.com>
2024-08-27 23:49:01 -0600

    Se anaden modificaciones que implementan la funcionalidad del encriptador y mejoras visuales en el archivo.

--- main.js
+++ main.js
@@ -1,30 +1,48 @@
-
 function encriptarTexto() {
-    // Lógica de encriptación (puedes reemplazar esto con la lógica real)
     const textoIngresado = document.querySelector('.presentacion-input-text').value;
-    const textoEncriptado = btoa(textoIngresado); // Esto es solo un ejemplo de encriptación básica
-
-    mostrarResultado(textoEncriptado);
+    if (textoIngresado === "") {
+        resetearVista(); // Llama a la función para restablecer la vista
+    } else {
+        const textoEncriptado = textoIngresado
+            .replace(/e/g, "enter")
+            .replace(/i/g, "imes")
+            .replace(/a/g, "ai")
+            .replace(/o/g, "ober")
+            .replace(/u/g, "ufat");
+        mostrarResultado(textoEncriptado);
+    }
 }
 
 function desencriptarTexto() {
-    // Lógica de desencriptación (puedes reemplazar esto con la lógica real)
     const textoIngresado = document.querySelector('.presentacion-input-text').value;
-    const textoDesencriptado = atob(textoIngresado); // Esto es solo un ejemplo de desencriptación básica
-
-    mostrarResultado(textoDesencriptado);
+    if (textoIngresado === "") {
+        resetearVista(); // Llama a la función para restablecer la vista
+    } else {
+        const textoDesencriptado = textoIngresado
+            .replace(/enter/g, "e")
+            .replace(/imes/g, "i")
+            .replace(/ai/g, "a")
+            .replace(/ober/g, "o")
+            .replace(/ufat/g, "u");
+        mostrarResultado(textoDesencriptado);
+    }
 }
 
 function mostrarResultado(texto) {
-    // Ocultar la imagen y el mensaje inicial
-    document.getElementById('illustration').style.display = 'none';
+    document.getElementById('illustrationNotFound').style.display = 'none';
     document.getElementById('no-message').style.display = 'none';
 
-    // Mostrar el resultado en la caja de texto
     const resultBox = document.getElementById('result-box');
     const resultText = document.getElementById('result-text');
     resultText.textContent = texto;
     resultBox.style.display = 'block';
+}
+
+function resetearVista() {
+    document.getElementById('illustrationNotFound').style.display = 'block';
+    document.getElementById('no-message').style.display = 'block';
+    document.getElementById('result-box').style.display = 'none';
+    document.querySelector('.presentacion-input-text').value = ""; // Opcional: borrar el texto ingresado
 }
 
 function copiarTexto() {
